3c52946e0288a6842cf0190993ff25414211b5f2
Ufuk Yüksel <ufukmirzayuksel@hotmail.com>
2021-12-13 19:19:43 +0300

    update_ShopCard

--- lib/features/addProduct/addProductView.dart
+++ lib/features/addProduct/addProductView.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hepsiyoldaapp/features/home/model/product_model.dart';
@@ -19,6 +20,12 @@ class AddProduct extends StatefulWidget{
 class AddProductView extends State<AddProduct> {
 
   File? _imageFile;
+  XFile? _pickedFile;
+   final controllerId = TextEditingController();
+   final controllerName = TextEditingController();
+   final controllerPrice = TextEditingController();
+   final controllerTotal = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
 
@@ -29,6 +36,7 @@ class AddProductView extends State<AddProduct> {
 
     Future<void> pickImage() async {
       final pickedFile = await picker.pickImage(source: ImageSource.camera);
+      _pickedFile=pickedFile;
       //Directory appDirectory = await getApplicationDocumentsDirectory();
       //File newImage = File(appDirectory.path + 'fileName');
       //newImage.writeAsBytes(File(pickedFile!.path).readAsBytesSync());
@@ -39,15 +47,15 @@ class AddProductView extends State<AddProduct> {
     }
 
 
-    Future<void> addProduct() {
+    Future<void> addProduct(Product product) {
       // Call the user's CollectionReference to add a new user
       return products
           .add({
-        'sId': "222", // John Doe
-        'name': "company", // Stokes and Sons
-        'image': "https://cdn.dsmcdn.com/mnresize/400/-/ty128/product/media/images/20210610/17/98485339/56271075/1/1_org.jpg",
-        'total':25,
-        'price':52,
+        'sId': product.sId, // John Doe
+        'name': product.name, // Stokes and Sons
+        'image': product.image,
+        'total':product.total,
+        'price':product.price,
       })
           .then((value) => print("User Added"))
           .catchError((error) => print("Failed to add user: $error"));
@@ -56,60 +64,104 @@ class AddProductView extends State<AddProduct> {
 
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       //var product=Product(sId: "132153", name: "ahmet", image: "", total: 20, price: 30);
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-
-          CircleAvatar(
-            child: Stack(
-                children: [
-                  ClipOval(
-                    child: (_imageFile != null)
-                        ? Image.file(_imageFile!, fit: BoxFit.cover,width:50)
-                        : Image.asset('images/newimage.png'),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: CircleAvatar(
-                      radius: 18,
-                      backgroundColor: Colors.white70,
-                      child: IconButton(icon: Icon(CupertinoIcons.camera), onPressed: () {
-                        pickImage();
-                      },),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.blue,
+//                backgroundColor: Color(0xff476cfb),
+                  child: ClipOval(
+                    child: new SizedBox(
+                      width: 160.0,
+                      height: 160.0,
+                      child: (_imageFile != null)
+                          ? Image.file(
+                        _imageFile!,
+                        fit: BoxFit.fill,
+                      )
+                          : Image.network(
+                        "https://images.unsplash.com/photo-1502164980785-f8aa41d53611?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                        fit: BoxFit.fill,
+                      ),
                     ),
                   ),
-                ]
-            ),
+                ),
+              ),
+           Padding(
+             padding: EdgeInsets.only(bottom: 40),
+             child: IconButton(
+               icon: Icon(
+               Icons.photo_camera,
+               size: 40,
+               ),
+               onPressed: () {
+                  pickImage();
+               },
+             ),
+           ),
+            ],
           ),
-          const Padding(
+           Padding(
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
             child: TextField(
+              controller: controllerId,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                hintText: 'Enter a search term',
+                hintText: 'Enter a product Id',
               ),
             ),
           ),
-          const Padding(
+           Padding(
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
             child: TextField(
+              controller:controllerName,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                hintText: 'Enter a search term',
+                hintText: 'Enter a Product Name',
               ),
             ),
           ),
-          const Padding(
+           Padding(
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
             child: TextField(
+              controller:controllerPrice,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                hintText: 'Enter a search term',
+                hintText: 'Enter a Product Price (just integer)',
               ),
             ),
           ),
-Text(_imageFile!=null ? _imageFile!.path:"sa")
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller:controllerTotal,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a Product Total (just integer)',
+              ),
+            ),
+          ),
+          Align(alignment:Alignment.bottomCenter,child:TextButton(onPressed: () async {
+             String downloadUrl="";
+            if ( _pickedFile!= null) {
+              var ref = FirebaseStorage.instance.ref();
+              var addImg = await ref.child("image/img").putFile(_imageFile!);
+              if (addImg != null) {
+                downloadUrl =  await addImg.ref.getDownloadURL();
+              }
+            }
+          await addProduct(Product(sId:controllerId.text,name:controllerName.text,image:downloadUrl,total:int.parse(controllerTotal.text),price:int.parse(controllerPrice.text)));
+            Navigator.pop(context);
+
+          },child: Text("Aaaa"),))
         ],
       ),
     );
